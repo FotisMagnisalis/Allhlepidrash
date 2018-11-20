7655fe78b5f9cbe93240b19ac7aa91dc004a5894
Fotios Magnisalis <fotis23able@gmail.com>
2018-11-20 05:00:10 +0200

    wELCOME

--- Allhlepidrash/Form1.Designer.cs
+++ Allhlepidrash/Form1.Designer.cs
@@ -28,24 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // start_button
             // 
-            this.button1.Location = new System.Drawing.Point(682, 310);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(451, 225);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.start_button.Location = new System.Drawing.Point(682, 310);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(451, 225);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "WELCOME";
+            this.start_button.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1856, 1179);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.start_button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +56,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button start_button;
     }
 }
 
