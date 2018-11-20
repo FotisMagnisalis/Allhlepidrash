e7fb3fea1337d23acd35c3f6a192e8f9143558a1
Fotios Magnisalis <fotis23able@gmail.com>
2018-11-20 19:14:08 +0200

    Evala titlo giorgaki.

--- Allhlepidrash/Form1.Designer.cs
+++ Allhlepidrash/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.start_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // start_button
@@ -43,22 +44,36 @@
             this.start_button.Text = "WELCOME";
             this.start_button.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(751, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1278, 226);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ΒΙΒΛΙΑΓΟΡΑ";
+            // 
             // Welcome_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2784, 1814);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.start_button);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Welcome_Form";
             this.Text = "Welcome";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label label1;
     }
 }
 
